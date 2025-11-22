5da309e4a69c5570da1f1234c2ac3aa54dbe3779
sujaywhitehat <sujaysarveshkk007@gmail.com>
2025-11-22 19:43:25 +0530

    Configure Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base:"/your-portfolio/"
-});
+  base: '/portfolio/',   // must match repo name
+})
